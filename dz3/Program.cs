2960123acc0e078c10431fc8d028b126f115f796
Надежда <@DESKTOP-EHSPKBR>
2024-11-21 00:40:30 +0200

    Добавьте файлы проекта.

---
+++ dz3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[]numbers = new int[15];
+Console.WriteLine("Введите 15 целых чисел");
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+int max = numbers.Max();
+int min = numbers.Min();
+int sum = max + min;
+int difference = max - min;
+Console.WriteLine($"Сумма максимального и минимального элиментов:{sum}");
+Console.WriteLine($"Разность максимальногои минимального элиментов:{difference}");
